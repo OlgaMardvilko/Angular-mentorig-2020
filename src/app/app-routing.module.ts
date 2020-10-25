1e275a3ab2d3d8633fcd71448228581303d16b1d
OlgaMardvilko <omardvilko@mail.ru>
2020-10-25 23:04:40 +0300

    added simple default routing and console for Lifescycle hooks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CoursesComponent } from './components/courses/courses.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/courses' },
+  { path: 'courses', component: CoursesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/courses/courses-list/courses-list.component.ts
+++ src/app/components/courses/courses-list/courses-list.component.ts
@@ -1,26 +1,55 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, DoCheck,
+  AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 import { ICourse } from '../../../models/course.model';
 
+// tslint:disable-next-line: no-conflicting-lifecycle
 @Component({
   selector: 'app-courses-list',
   templateUrl: './courses-list.component.html',
   styleUrls: ['./courses-list.component.scss']
 })
 
-export class CoursesListComponent implements OnInit, OnChanges {
+export class CoursesListComponent implements OnInit, OnChanges,
+  DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
   public coursesList: ICourse[] = [];
 
   @Input() courses: ICourse[];
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
-
   ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges Lifecycle hook');
     if (changes.courses) {
       this.coursesList = changes.courses.currentValue;
     }
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit Lifecycle hook');
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck Lifecycle hook');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit Lifecycle hook');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked Lifecycle hook');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit Lifecycle hook');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked Lifecycle hook');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy Lifecycle hook');
   }
 
   onEditCourse(courseId: string): void {
